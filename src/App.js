6343ad8d9588383858b309529e7ec67ebb0cc22b
LafaDev <lafa.dev@protonmail.com>
2022-02-06 15:09:30 -0500

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" render={ () => (<Login />) } />
+          <Route path="/search" render={ () => (<Search />) } />
+          <Route path="/album" render={ () => (<Album />) } />
+          <Route path="/favorites" render={ () => (<Favorites />) } />
+          <Route path="/profile/edit" render={ () => (<ProfileEdit />) } />
+          <Route exact path="/profile" render={ () => (<Profile />) } />
+          <Route exact path="*" render={ () => (<NotFound />) } />
+        </Switch>
+      </Router>
+    );
   }
 }
 
---
+++ src/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-not-found">
+        <h1>NotFound</h1>
+      </div>
+    );
+  }
+}
+export default NotFound;
