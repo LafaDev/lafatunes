4e76d9b72daae00ed332191875b278dd863633a5
LafaDev <lafa.dev@protonmail.com>
2022-02-07 03:43:58 -0500

    Requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 
 class Header extends React.Component {
@@ -32,6 +33,19 @@ class Header extends React.Component {
         <h1>Header</h1>
         {loading ? (<p>Carregando...</p>) : null}
         <h2 data-testid="header-user-name">{user.name}</h2>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/search" data-testid="link-to-search">Search</Link>
+            </li>
+            <li>
+              <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+            </li>
+            <li>
+              <Link to="/profile" data-testid="link-to-profile">Profile</Link>
+            </li>
+          </ul>
+        </nav>
       </header>
     );
   }
