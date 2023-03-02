4b0df82f8d87bcae0860e20ca0f3cfb3a839108a
lafadev <lafa.dev@protonmail.com>
2023-03-02 11:43:35 -0300

    fetch error handler

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -3,11 +3,13 @@ import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import getMusics from '../services/musicsAPI';
 import MusicCard from '../components/MusicCard';
+import loadingGif from '../images/headphone.gif';
 
 class Album extends React.Component {
   constructor() {
     super();
     this.state = {
+      error: false,
       loading: false,
       musics: [],
       artistName: '',
@@ -26,25 +28,41 @@ class Album extends React.Component {
       { loading: true },
       async () => {
         const result = await getMusics(id);
-        this.setState(
-          {
-            loading: false,
-            musics: [...result],
-            artistName: result.find((e) => e).artistName,
-            collectionName: result.find((e) => e).collectionName,
-          },
-        );
+        if (result) {
+          this.setState(
+            {
+              loading: false,
+              musics: [...result],
+              artistName: result.find((e) => e).artistName,
+              collectionName: result.find((e) => e).collectionName,
+            },
+          );
+        } else {
+          this.setState({ error: true });
+        }
       },
     );
   }
 
   render() {
-    const { loading, artistName, collectionName, musics } = this.state;
+    const { loading, artistName, collectionName, musics, error } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
         { loading ? <h1>Carregando...</h1> : null}
         <h1 data-testid="artist-name">{ artistName }</h1>
+        { error ? (
+          <div>
+            <img
+              src={ loadingGif }
+              alt="Headphones animation of loading"
+              className="errorgif"
+              width="180"
+              height="180"
+            />
+            <h1>Não foi possivel encontrar esse album</h1>
+          </div>
+        ) : null}
         <h1 data-testid="album-name">{ collectionName }</h1>
         <ul className="musicList">
           {musics.slice(1).map(((e) => (
